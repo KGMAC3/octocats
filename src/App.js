8791d2d139f55bec3cf7c368e1326d7836a8a8a9
KGMAC3 <kgmac3@gmail.com>
2019-09-27 18:03:22 -0400

    test 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { Component } from "react"
+import HelloWorld from "./components/HelloWorld"
+import Cats from "./components/Cats"
+import Header from "./components/Header"
+import FavoriteCats from "./components/FavoriteCats"
+import CatProp from "./components/CatProp"
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <section>
+          <Header />
+        </section>
+
+        {/* <Cats /> */}
+        <main class='top-header'>
+          <FavoriteCats />
+        </main>
+      </>
+    )
   }
 }
 
